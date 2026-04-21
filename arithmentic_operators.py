572b3521d91b5f4e57dfa2677e2e1d6268b5094a
Flonicah Leonard <95224392+Flonicah@users.noreply.github.com>
2026-04-21 07:40:06 +0000

    arithmetical operators in python

--- arithmentic_operators.py
+++ arithmentic_operators.py
@@ -8,3 +8,22 @@ print(10 +3)
 
 #substraction operator(-)
 print(10-3)
+
+# the multiplication * operator
+print(10*3)
+
+# the division / operator
+print(10/3)
+
+#the floor (//)operator
+#example
+print(10//3)
+print(10//2)
+
+# the modulo % operator returns the reminder after division
+print(10%3)
+
+#expenential ** operator power of inshort
+#example
+print(2**3)
+print(2**2)
