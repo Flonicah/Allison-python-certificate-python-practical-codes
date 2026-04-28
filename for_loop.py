3157728598734d60ea2d11bc86b0623969644d24
Flonicah Leonard <95224392+Flonicah@users.noreply.github.com>
2026-04-28 09:53:25 +0000

    continuation on python practical learning

---
+++ for_loop.py
@@ -0,0 +1,24 @@
+"""
+LOOPS 
+THE FOR LOOP
+
+for i in range (5):
+    print("Python for loops programming that means repeating")
+
+#another example of the for loop programming
+
+for i in range (1,11,2):
+    print(i) #prints the number in i which are 3,5,7,9 print numbers from 1 to 11 with an increment 
+
+#another example
+
+name=("Python")
+for i in name:
+    print(i) # this prints the python in name stored  variable
+
+for i in range(10):
+    print(i)
+"""
+for i in range(11):
+    if i > 0:
+        print(i)
---
+++ the_whileloop.py
@@ -0,0 +1,15 @@
+"""
+The while loop used to repeat an action until a task is satisfield or meet
+
+"""
+count = 1
+
+while count <=5:
+    print("python")
+    count+=1
+
+#another example
+pasfloxysword= ""
+while password!= "computer":
+    password=input("Enter Password: \n")
+print("Access granted")
